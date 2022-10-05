9aae13726f0fe529fe2b4e39a30eb717b4b548ec
Abhinav Jain <abhinavjainn412@gmail.com>
2022-10-05 11:58:46 +0530

    Add files via upload

---
+++ caesar.py
@@ -0,0 +1,14 @@
+import string
+
+def caesar(text, shift,alphabets):
+    def shift_alphabet (alphabet) :
+        return alphabet [shift:] + alphabet[:shift]
+    shifted_alphabets=tuple(map(shift_alphabet,alphabets))
+    final_alphabet =''.join(alphabets)
+    final_shifted_alphabet =''.join (shifted_alphabets)
+    table = str.maketrans(final_alphabet,final_shifted_alphabet)
+    return text. translate(table)
+plain_text="AB"
+plain_textq="BC"
+print(caesar(plain_text,1,[string.ascii_lowercase,string.ascii_uppercase,string.punctuation]))
+print(caesar(plain_textq,26-1,[string.ascii_lowercase,string.ascii_uppercase,string.punctuation]))
